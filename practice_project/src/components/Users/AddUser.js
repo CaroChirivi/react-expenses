551b3ab2411071d0abf0db52db45f5bf530c0a3c
CaroChirivi <carotarapues@gmail.com>
2022-02-18 12:05:01 -0500

    Se crea componente Card

---
+++ practice_project/src/components/Users/AddUser.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import Card from "../UI/Card";
+
+import styles from './AddUser.module.css'
+
+const AddUser = props => {
+    const addUserHandler = (event) => {
+        event.preventDefault();
+    }
+
+    return (
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text"></input>
+                <label htmlFor="ge">Age (Years)</label>
+                <input id="age" type="number"></input>
+                <button type="submit">Add user</button>
+            </form>
+        </Card>
+    )
+}
+
+export default AddUser;
---
+++ practice_project/src/components/Users/Users.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Card from "../UI/Card";
+
+const Users = () => {
+    return (
+        <Card></Card>
+     )
+}
+
+export default Users
